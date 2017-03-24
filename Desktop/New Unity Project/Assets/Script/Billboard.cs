19ac2315da5a7b35f72e816227b36dc3bf6cd09e
chenzehao <chenzehao@brcs.cn>
2017-03-24 14:31:30 +0800

    3.24

---
+++ Desktop/New Unity Project/Assets/Script/Billboard.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//
+//限制公告的字数 字数 = 长度／滚动速度
+//
+public class GetNotice : MonoBehaviour {
+	public UILabel billlabel;
+	public string url = "119.23.145.192:3010/notice";
+	List<string> noticeStack_title = new List<string>();		//公告标题集合	
+	List<string> noticeStack_msg = new List<string>();		//公告信息集合
+	public string token;
+	
+	///<summary>
+	///存放可选参数
+	///</summary>
+	public enum type{};
+	public uint gid;
+	public uint rid;
+
+	NoticeModel notice = new NoticeModel();
+
+	void Start () 
+	{
+		getNotice(token);
+	}
+
+	void Update () 
+	{
+		getNotice(token);
+	}
+
+	///<summary>
+	///获取公告信息
+	///</summary>
+	private void getNotice(string token)
+	{
+		StartCoroutine(Get(token));
+	}
+
+	private IEnumerator Get(string token , enum type=lobby , uint gid=null , uint rid=null)
+	{
+		string getUrl = url + "?" +"token="	+ tokne + "&type=" + type + "&gid=" + gid + "&rid=" + rid;
+		WWW www = new WWW(getUrl);
+		yield return www;
+		if (www.error != null)
+		{
+			//错误信息处理
+			return false;
+		}
+
+		//从服务器返回的信息提取返回码
+		string str = www.text;
+		string result = System.Text.RegularExpressions.Regex.Replace(str, @"[^0-9]+", "");
+		if (result != "200")
+		{
+			switch (result)
+			{
+				case "500":
+					Debug.Log("FAIL");
+				case "2001":
+					Debug.Log("FA_NO_CONNECTOR_SERVER");
+				case "3001":
+					Debug.Log("FA_ENTER_ERROR");
+				case "3002":
+					Debug.Log("FA_CONNECTED");
+			}
+			return false;
+		}
+		else
+		{
+			///<summary>
+			///拉取公告栏的信息，保存到 NoticeModel notice;
+			///</summary>
+			List<NoticeModel> _notice =  JsonConvert.DeserializeObject(List<NoticeModel>)(www.text);
+			notice.ID = _notice.ID;
+			notice.Title = _notice.Title;
+			notice.Msg = _notice.Msg;
+			notice.Type = _notice.Type;
+			notice.GID = _notice.GID;
+			notice.RID = _notice.RID;
+			notice.IsShow = _notice.IsShow;
+			
+			//将获取到的信息保存在集合中，供滚动信息调用
+			addNotice(notice.Title,notice.Msg);
+		}
+	}
+
+	///<summary>
+	///将获取到的消息保存起来
+	///</summary>
+	public void addNotice(string title,string message)
+	{
+		if(title!="" && message!="")
+		{
+			noticeStack_title.Add(title);
+			noticeStack_msg.Add(message);
+		}
+	}
+
+	///<summary>
+	///滚动信息的函数
+	///</summary>
+	public void rollNotice(List<string> title,List<string> msg)
+	{
+		billlabel.text = title[0] + msg[0];
+	}
+}
---
+++ Desktop/New Unity Project/Assets/Script/login.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+//使用newtonsoft解析json
+// using Newtonsoft.Json; 
+
+public class Login : MonoBehaviour {
+	public string username = "zdcgh";
+	public string password = "hello";
+	//登录失败的提示信息
+	public string prom_msg = "";
+	public static string postUrl = "119.23.145.192:3010/login";
+	///<summary>
+	///定义用来存放用户信息的对象
+	///</summary>
+	// SysInfoModel sysInfo = new SysInfoModel();
+	// UserInfoModel userInfo = new UserInfoModel();
+
+
+	void onClick()
+	{
+		login(username,password);
+	}
+	void onGUI()
+	{
+		//登录场景中错误的提示处
+		GUI.Label(new Rect(0,0,0,0),prom_msg);
+	}
+
+
+	private void login(string Username,string Password)
+	{
+        StartCoroutine(PostMessages(Username,Password));
+	}
+
+	 private IEnumerator PostMessages(string UserName,string PassWord)
+	 {
+		WWWForm form = new WWWForm();
+        form.AddField("username" , UserName);
+		form.AddField("password",PassWord);
+        WWW www = new WWW(postUrl,form);
+        yield return www;
+		if(www.error != null)
+		{
+			//错误信息提示，暂时不知道错误信息的格式
+			//超时连接，没有网络等情况
+			return false;
+		}
+
+		//登录失败
+		//从服务器返回的信息提取返回码
+		string str = www.text;
+		string result = System.Text.RegularExpressions.Regex.Replace(str, @"[^0-9]+", "");
+		if(result != "200")
+		{
+			switch(result)
+            {
+                case "501" : 
+                    Debug.Log("参数错误");
+                    break;
+				case "-1" :
+					prom_msg = "登录失败，请重新登录";
+					break;
+                case "-2" :
+                    prom_msg = "用户名未找到，请检查用户名是否正确";
+                    break;
+                case "-3" :
+                    prom_msg = "密码输入错误，请重新输入";
+                    break;
+                case "-4" :
+                    prom_msg = "用户被锁";
+                    break;
+                case "-5" : 
+                    prom_msg = "用户已经在在线";
+                    break;
+            }
+            return false;
+		}
+		//登录成功
+		else
+		{
+			///<summary>
+			///将用户信息保存在 UserInfoModel userInfo中
+			///使用newtonsoft解析服务器返回的json
+			///</summary>
+			// List<UserInfoModel> _userInfo = JsonConvert.DeserializeObject(List<UserInfoModel>)(www.text);
+			// userInfo.ID = _userInfo.ID;
+			// userInfo.UID = _userInfo.UID;
+			// userInfo.Gaccount = _userInfo.Gaccount;
+			// userInfo.Score = _userInfo.Score;
+			// userInfo.ScoreFree = _userInfo.ScoreFree;
+			// userInfo.ScoreBank = _userInfo.ScoreBank;
+			// userInfo.IsOn = _userInfo.IsOn;
+			// userInfo.Token = _userInfo.Token;
+			//加载游戏大厅
+			// Application.LoadLevel("HallWindow");	
+
+
+			//检测版本
+			// VersionTest(userInfo.Token);
+		}
+	 }
+
+	 #region 获取系统信息，游戏版本等 GET
+//		private void VersionTest(string token)
+//		{
+//      	 	 StartCoroutine(SysInfo(token));
+//		}
+//		private IEnumerator SysInfo(string token)
+//		{
+//			WWW www = new WWW(postUrl);
+//			yield return www;
+//			if (www.error != null)
+//			{
+//				//Get请求失败
+//			}
+//			else
+//			{
+//				//Get请求成功
+//				
+//				//解析数据
+//			}
+//		}
+	 #endregion
+}
