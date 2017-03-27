c695c7c18b1092f80574e94d6e7db3770fb01329
Your Real Name <chenzehao@brcs.cn>
2017-03-27 10:02:32 +0800

    登录，版本更新，公告栏

---
+++ Downloads/demo/Script/http_login.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleJson;
+
+public class http_login : sysInfo {
+	public UILabel IUsername;
+	public UILabel IPassword;
+	public string prom_user = "";		//用户名错误时的提示信息
+	public string prom_psw = "";		//密码输入错误时的提示信息
+	public static string userInfo_url = "119.23.145.192:3010/login";
+	public static string sysInfo_url = "119.23.145.192:3010/sysInfo";
+
+	///<summary>
+	///定义用来存放用户信息的对象
+	///protobuf未能成功转化，会报错
+	///</summary>
+	SysInfoModel sysInfo = new SysInfoModel();
+	UserInfoModel userInfo = new UserInfoModel();
+	
+	void Awake()
+	{
+		GameObject button = GameObject.Find("UI Root/LoginWindow/Button_Entry");
+		UIEventListener.Get(button).onClick = OnEntryClick;
+	}
+
+	void OnEntryClick(GameObject go)
+	{
+		login(IUsername.text,IPassword.text);
+	}
+
+	void onGUI()
+	{
+		//登录中错误信息的提示框
+		//具体的坐标还未定为
+		//在不同的屏幕中定位的坐标是否不同
+		GUI.Label(new Rect(111,156,266,674),prom_user);
+		GUI.Label(new Rect(0,0,0,0),prom_psw);
+	}
+	
+	
+	private void login(string Username,string Password)
+	{
+		StartCoroutine(PostMessages(Username,Password));
+	}
+	
+	private IEnumerator PostMessages(string UserName,string PassWord)
+	{
+		Debug.Log(PassWord);
+		WWWForm form = new WWWForm();
+		form.AddField("username",UserName);
+		form.AddField("password",PassWord);
+		WWW www = new WWW(userInfo_url,form);
+		yield return www;
+		if(www.error != null)
+		{
+			//错误信息提示，暂时不知道错误信息的格式
+			//超时连接，没有网络等情况
+			Debug.Log("登录失败，请检查网络是否正常");
+			return false;
+		}
+		
+		//登录失败
+		//从服务器返回的信息提取返回码
+		string str = www.text;
+		string result = System.Text.RegularExpressions.Regex.Replace(str, @"[^0-9]+", "");
+		if(result != "200")
+		{
+			Debug.Log(www.text);
+			switch(result)
+			{
+			case "500" : 
+				Debug.Log("登录失败");
+				break;
+			case "501" : 
+				Debug.Log("参数错误");
+				break;
+			case "-1" :
+				prom_msg = "登录失败，请重新登录";
+				break;
+			case "-2" :
+				prom_msg = "用户名未找到，请检查用户名是否正确";
+				break;
+			case "-3" :
+				prom_msg = "密码输入错误，请重新输入";
+				break;
+			case "-4" :
+				prom_msg = "用户被锁";
+				break;
+			case "-5" : 
+				prom_msg = "用户已经在在线";
+				break;
+			}
+			return false;
+		}
+		//登录成功
+		else
+		{
+			Debug.Log("登录成功");
+			///<summary>
+			///将用户信息保存在 UserInfoModel userInfo中
+			///使用simpleJson解析服务器返回的json
+			///</summary>
+			parseJson(www.text);
+		}
+	}
+
+
+	#region 解析json,将数据存放在userInfo对象中
+	private GameObject parseJson(string jsonData)
+	{
+		string json = JSON.Parse(jsonData);
+		if(json["code"] == "200")
+		{
+			userInfo.id = json["data"]["user"]["id"];
+			userInfo.uid = json["data"]["user"]["uid"];
+			userInfo.gaccount = json["data"]["user"]["gaccount"];
+			userInfo.score = json["data"]["user"]["score"];
+			userInfo.score_free = json["data"]["user"]["score_free"];
+			userInfo.score_bank = json["data"]["user"]["score_bank"];
+			userInfo.is_on = json["data"]["user"]["is_on"];
+			userInfo.token = json["data"]["token"];
+			return userInfo;
+		}
+	}
+	#endregion
+	
+
+
+
+}
---
+++ Downloads/demo/Script/http_notice.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+
+///<summary>
+///限制公告的字数 字数 = 长度／滚动速度
+///</summary>
+
+public class http_notice : MonoBehaviour {
+	public UILabel billlabel;									//滚动栏的文字
+	public string notice_url = "119.23.145.192:3010/notice";	//请求链接
+	List<string> noticeStack_title = new List<string>();		//公告标题集合	
+	List<string> noticeStack_msg = new List<string>();			//公告信息集合
+	public const int SPEED;										//固定的滚动速度
+	public uint boundary;										//滚动字幕的边界
+	public string token;
+	
+	///Get函数的可选参数
+	public enum type{type = lobby,};
+	public uint gid;
+	public uint rid;
+
+	///<summary>
+	///notice对象用来存放信息
+	///</summary>
+	NoticeModel notice = new NoticeModel();
+
+	void Update () 
+	{
+		getNotice(token);
+		while (noticeStack_msg[0] == null || noticeStack_title[0]==null){}
+		//滚动信息
+		rollNotice(noticeStack_title,noticeStack_msg);
+		//删除信息
+		removeNotice(noticeStack_title,noticeStack_msg);
+	}
+
+	///<summary>
+	///获取公告信息
+	///</summary>
+	private void getNotice(string token)
+	{
+		StartCoroutine(Get(token));
+	}
+
+	private IEnumerator Get(string token , enum type=lobby , uint gid=null , uint rid=null)
+	{
+		//需要是%编码 
+		string getUrl = notice_url + "?" +"token="	+ tokne + "&type=" + type + "&gid=" + gid + "&rid=" + rid;
+		WWW www = new WWW(getUrl);
+		yield return www;
+		if (www.error != null)
+		{
+			//错误信息处理
+			Debug.Log("公告栏获取失败");
+			return false;
+		}
+
+		//从服务器返回的信息提取返回码
+		string str = www.text;
+		string result = System.Text.RegularExpressions.Regex.Replace(str, @"[^0-9]+", "");
+		if (result != "200")
+		{
+			switch (result)
+			{
+				case "500":
+					Debug.Log("FAIL");
+				case "2001":
+					Debug.Log("FA_NO_CONNECTOR_SERVER");
+				case "3001":
+					Debug.Log("FA_ENTER_ERROR");
+				case "3002":
+					Debug.Log("FA_CONNECTED");
+			}
+			return false;
+		}
+		else
+		{
+			//解析数据
+			ParseJson(www.text);
+			//将获取到的信息保存在集合中，供滚动信息调用
+			addNotice(notice.title,notice.msg);
+		}
+	}
+
+
+	#region 解析json,添加信息，展示信息，移除信息
+
+	///<summary>
+	///解析json并把数据存放在notice对象中
+	///</summary>
+	private GameObject ParseJson(string jsonData)
+	{
+		if(notice["code"] == 200)
+		{
+			string json = Json.Parse(jsonData);
+			notice.id = json["data"]["notice"]["id"];
+			notice.title = json["data"]["notice"]["title"];
+			notice.msg = json["data"]["notice"]["msg"];
+			notice.type = json["data"]["notice"]["type"];
+			notice.gid = json["data"]["notice"]["gid"];
+			notice.rid = json["data"]["notice"]["rid"];
+			notice.is_show = json["data"]["notice"]["is_show"];
+			return notice;
+		}
+		else
+		{
+			Debug.Log("解析json错误");
+			return false;
+		}
+	}
+
+	///<summary>
+	///将获取到的消息保存起来
+	///</summary>
+	public void addNotice(string title,string message)
+	{
+		if(title!="" && message!="")
+		{
+			noticeStack_title.Add(title);
+			noticeStack_msg.Add(message);
+		}
+	}
+
+	///<summary>
+	///滚动信息的函数
+	///</summary>
+	public void rollNotice(List<string> title,List<string> msg)
+	{
+		billlabel.text = title[0] + ": " + msg[0];
+		//这里的lefttop并不是正确的写法
+		if(billlabel.text.leftpos>boundary)
+		{
+			billlabel.text.leftpos -= Time.deltaTime * SPEED;
+		}
+
+	}
+
+	///<summary>
+	///移除已经显示过的信息
+	///</summary>
+	public void removeNotice(List<string> title,List<string> msg)
+	{
+		title.remove(0);
+		msg.remove(0;)
+	}
+
+	#endregion
+
+}
+
